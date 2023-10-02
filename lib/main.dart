f4dad13c0a72142e35df2d1f58827be8a574092b
ziehro <56759812+ziehro@users.noreply.github.com>
2023-10-02 00:43:22 -0600

    initial home commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,23 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:location/location.dart';
 import 'dart:math';
-
+import 'dart:async';
 import 'firebase_options.dart';
+import 'package:flutter/foundation.dart';  // Add this import for kIsWeb
+import 'package:url_launcher/url_launcher.dart';
+import 'src/url_launcher/url_launcher_stub.dart'
+if (dart.library.js) 'src/url_launcher/url_launcher_web.dart';
+
+
+
+
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();  // Ensure the Flutter widgets binding is initialized.
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-
   runApp(MyApp());
 }
 
@@ -32,134 +40,145 @@ class BeachListScreen extends StatefulWidget {
 }
 
 class _BeachListScreenState extends State<BeachListScreen> {
-  Location location = new Location();
-  bool? _serviceEnabled;
-  PermissionStatus? _permissionGranted;
+  final Location location = Location();
   LocationData? _locationData;
+  final StreamController<List<DocumentSnapshot>> _streamController = StreamController<List<DocumentSnapshot>>();
 
   @override
   void initState() {
     super.initState();
-    _getLocation();
+    _getLocation().then((_) {
+      _streamController.addStream(getNearbyBeaches());
+    });
   }
 
-  Future<void> _getLocation() async {
-    try {
-      _serviceEnabled = await location.serviceEnabled();
-      if (!_serviceEnabled!) {
-        _serviceEnabled = await location.requestService();
-        if (!_serviceEnabled!) {
-          // Inform the user about the importance of enabling location services.
-          ScaffoldMessenger.of(context).showSnackBar(
-              SnackBar(content: Text('Location services are required!')));
-          return;
-        }
-      }
+  void _launchMapsUrl(double latitude, double longitude) async {
+    String googleMapsUrl = "https://www.google.com/maps/search/?api=1&query=$latitude,$longitude";
 
-      _permissionGranted = await location.hasPermission();
-      if (_permissionGranted == PermissionStatus.denied) {
-        _permissionGranted = await location.requestPermission();
-        if (_permissionGranted != PermissionStatus.granted) {
-          // Inform the user about the importance of granting location permissions.
-          ScaffoldMessenger.of(context).showSnackBar(
-              SnackBar(content: Text('Location permissions are required!')));
-          return;
-        }
+    if (kIsWeb) {
+      launchMapsUrl; // This will open a new browser tab
+    } else {
+      if (await canLaunch(googleMapsUrl)) {
+        await launch(googleMapsUrl);
+      } else {
+        throw 'Could not launch Google Maps';
       }
-
-      _locationData = await location.getLocation();
-      print("Fetched Location: ${_locationData?.latitude}, ${_locationData?.longitude}");
-    } catch (e) {
-      // Handle any potential errors
-      print("An error occurred: $e");
-      ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(content: Text('An error occurred while fetching location!')));
     }
   }
 
 
-  Stream<List<DocumentSnapshot>> getNearbyBeaches() async* {
-    await _getLocation(); // Make sure to fetch location before processing beaches.
-    yield* FirebaseFirestore.instance
-        .collection('locations')
-        .snapshots()
-        .map((snapshot) {
-      List<DocumentSnapshot> sortedDocs = snapshot.docs.toList()
-        ..sort((a, b) {
-          double distanceA = calculateDistance(
-            _locationData?.latitude ?? 0.0,
-            _locationData?.longitude ?? 0.0,
-            (a.data() as Map<String, dynamic>)['latitude'].toDouble(),
-            (a.data() as Map<String, dynamic>)['longitude'].toDouble(),
-          );
-          double distanceB = calculateDistance(
-            _locationData?.latitude ?? 0.0,
-            _locationData?.longitude ?? 0.0,
-            (b.data() as Map<String, dynamic>)['latitude'].toDouble(),
-            (b.data() as Map<String, dynamic>)['longitude'].toDouble(),
-          );
-          return distanceA.compareTo(distanceB);
-        });
-      return sortedDocs.take(10).toList(); // Only take the top 3 nearest beaches.
+
+  Future<void> _getLocation() async {
+    try {
+      bool serviceEnabled = await location.serviceEnabled();
+      if (!serviceEnabled) {
+        serviceEnabled = await location.requestService();
+        if (!serviceEnabled) {
+          return; // Handle this case as needed.
+        }
+      }
+
+      PermissionStatus permissionGranted = await location.hasPermission();
+      if (permissionGranted == PermissionStatus.denied) {
+        permissionGranted = await location.requestPermission();
+        if (permissionGranted != PermissionStatus.granted) {
+          return; // Handle this case as needed.
+        }
+      }
+
+      _locationData = await location.getLocation();
+      setState(() {}); // Trigger a UI refresh now that we have location.
+    } catch (e) {
+      print("Error fetching location: $e");
+    }
+  }
+
+  Stream<List<DocumentSnapshot>> getNearbyBeaches() {
+    return FirebaseFirestore.instance.collection('locations').snapshots().map((snapshot) {
+      var sortedDocs = snapshot.docs.toList()
+        ..sort((a, b) => _compareDistance(a, b));
+      return sortedDocs.take(5).toList();
     });
   }
 
-
-
-
-
-  double calculateDistance(double lat1, double lon1, double lat2, double lon2) {
-    var p = 0.017453292519943295;
-    var c = cos;
-    var a = 0.5 -
-        c((lat2 - lat1) * p) / 2 +
-        c(lat1 * p) * c(lat2 * p) * (1 - c((lon2 - lon1) * p)) / 2;
-    return 12742 * asin(sqrt(a));
+  int _compareDistance(DocumentSnapshot a, DocumentSnapshot b) {
+    double distanceA = _calculateDistanceFromUser(a);
+    double distanceB = _calculateDistanceFromUser(b);
+    return distanceA.compareTo(distanceB);
   }
 
+  double _calculateDistanceFromUser(DocumentSnapshot doc) {
+    Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
+    return _calculateDistance(
+      _locationData?.latitude ?? 0,
+      _locationData?.longitude ?? 0,
+      data['latitude'],
+      data['longitude'],
+    );
+  }
+
+  double _calculateDistance(double lat1, double lon1, double lat2, double lon2) {
+    var p = 0.017453292519943295; // Pi/180
+    var a = 0.5 - cos((lat2 - lat1) * p) / 2 +
+        cos(lat1 * p) * cos(lat2 * p) * (1 - cos((lon2 - lon1) * p)) / 2;
+    return 12742 * asin(sqrt(a));
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Nearby Beaches"),
-      ),
+      appBar: AppBar(title: Text("Nearby Beaches")),
       body: StreamBuilder<List<DocumentSnapshot>>(
-        stream: getNearbyBeaches(),
-        builder: (BuildContext context, AsyncSnapshot<List<DocumentSnapshot>> snapshot) {
-          if (snapshot.hasError) {
-            return Center(child: Text('Something went wrong'));
-          }
+        stream: _streamController.stream,
+        builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
             return Center(child: CircularProgressIndicator());
           }
-          print(snapshot.data);
 
-          return ListView(
-            children: (snapshot.data ?? []).map((DocumentSnapshot document) {
-              Map<String, dynamic> data = document.data() as Map<String, dynamic>;
-              return ListTile(
-                contentPadding: EdgeInsets.all(8.0),
-                leading: Image.network(
-                  data['imageUrls'] != null && data['imageUrls'].isNotEmpty
-                      ? data['imageUrls'][0]
-                      : 'placeholder_image_url',  // replace with your placeholder image URL
-                  width: 100,  // Adjust width as per your requirement
-                  height: 100, // Adjust height as per your requirement
-                  fit: BoxFit.cover,
-                ),
-                title: Text(data['name']),
+          if (snapshot.hasError || !snapshot.hasData) {
+            return Center(child: Text('Error loading beaches.'));
+          }
+
+          return ListView.builder(
+            itemCount: snapshot.data?.length ?? 0,
+            itemBuilder: (context, index) {
+              var beach = snapshot.data![index];
+              var data = beach.data() as Map<String, dynamic>;
+
+              return InkWell(
                 onTap: () {
                   showDialog(
                     context: context,
                     builder: (context) {
                       return AlertDialog(
                         title: Text(data['name']),
-                        content: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Image.network(data['imageUrls'][0]),
-                            Text('Description: ${data['description']}'),
-                          ],
+                        content: SingleChildScrollView(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Container(
+                                height: 200,
+                                child: CarouselSlider(
+                                  options: CarouselOptions(
+                                    aspectRatio: 16 / 9,
+                                    viewportFraction: 0.8,
+                                    enlargeCenterPage: true,
+                                    autoPlay: true,
+                                  ),
+                                  items: (data['imageUrls'] as List).map((item) {
+                                    return Container(
+                                      child: Image.network(item, fit: BoxFit.cover),
+                                    );
+                                  }).toList(),
+                                ),
+                              ),
+                              SizedBox(height: 20),
+                              Text('Description: ${data['description']}'),
+                              SizedBox(height: 10),
+                              Text('Firewood: ${data['Firewood']}'),
+                              SizedBox(height: 10),
+                              Text('Sand: ${data['Sand']}'),
+                            ],
+                          ),
                         ),
                         actions: [
                           TextButton(
@@ -173,11 +192,116 @@ class _BeachListScreenState extends State<BeachListScreen> {
                     },
                   );
                 },
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+                  child: Column(
+                    children: [
+                      Container(
+                        width: kIsWeb ? 200 : 100,
+                        height: kIsWeb ? 200 : 100,
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            fit: BoxFit.cover,
+                            image: NetworkImage(
+                              data['imageUrls'] != null && data['imageUrls'].isNotEmpty
+                                  ? data['imageUrls'][0]
+                                  : 'placeholder_image_url',
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      Text(
+                        data['name'],
+                        style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          IconButton(
+                            icon: Icon(Icons.info_outline),
+                            onPressed: () {
+                              _showBeachDialog(data);
+                            },
+                          ),
+                          IconButton(
+                            icon: Icon(Icons.directions),
+                            onPressed: () {
+                              _launchMapsUrl(
+                                data['latitude'],
+                                data['longitude'],
+                              );
+                            },
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
               );
-            }).toList(),
+            },
           );
         },
       ),
     );
   }
+
+  @override
+  void dispose() {
+    _streamController.close();
+    super.dispose();
+  }
+
+  void _showBeachDialog(Map<String, dynamic> data) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text(data['name']),
+          content: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  height: 200,
+                  child: CarouselSlider(
+                    options: CarouselOptions(
+                      aspectRatio: 16 / 9,
+                      viewportFraction: 0.8,
+                      enlargeCenterPage: true,
+                      autoPlay: true,
+                    ),
+                    items: (data['imageUrls'] as List).map((item) {
+                      return Container(
+                        child: Image.network(item, fit: BoxFit.cover),
+                      );
+                    }).toList(),
+                  ),
+                ),
+                SizedBox(height: 20),
+                Text('Description: ${data['description']}'),
+                SizedBox(height: 10),
+                Text('Firewood: ${data['Firewood']}'),
+                SizedBox(height: 10),
+                Text('Sand: ${data['Sand']}'),
+                // Add other beach information here as needed
+              ],
+            ),
+          ),
+          actions: [
+            TextButton(
+              child: Text('Close'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+
+
 }
+
---
+++ lib/src/url_launcher/url_launcher_stub.dart
@@ -0,0 +1,3 @@
+void launchMapsUrl(String url) {
+  throw UnimplementedError('This function is implemented in url_launcher_web.dart');
+}
